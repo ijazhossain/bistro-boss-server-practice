02ab891a8c2e1dc755c016f302845201e521ff31
ijaz35 <ijaz.hossain1@gmail.com>
2023-06-05 11:18:02 +0600

    menu relates api added

--- index.js
+++ index.js
@@ -42,12 +42,58 @@ async function run() {
         const menuCollection = client.db('restaurantDB').collection('menu')
         const usersCollection = client.db('restaurantDB').collection('user')
         const cartCollection = client.db('restaurantDB').collection('cart')
+        // admin verify
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next();
+        }
         // Get JWT token
         app.post('/jwt', (req, res) => {
             const user = req.body
             // console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
             res.send({ token })
+        })
+
+        // user related API
+
+        app.get('/users', verifyJwt, verifyAdmin, async (req, res) => {
+            const result = await usersCollection.find({}).toArray();
+            res.send(result);
+        })
+        /* admin check */
+        app.get('/users/admin/:email', verifyJwt, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                res.send({ admin: 'false' })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const result = { admin: user?.role === 'admin' }
+            res.send(result)
+        })
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result);
+        })
+        app.delete('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const result = await usersCollection.deleteOne(query)
+            res.send(result);
         })
         // cart related API
         app.post('/cart', async (req, res) => {
@@ -58,7 +104,7 @@ async function run() {
         })
         app.get('/cart', verifyJwt, async (req, res) => {
             const email = req.query.email;
-            console.log(email);
+            // console.log(email);
             if (!email) {
                 res.send([]);
             }
@@ -69,7 +115,7 @@ async function run() {
             }
 
             const query = { email: email }
-            const result = await cartCollection.find({}).toArray();
+            const result = await cartCollection.find(query).toArray();
             res.send(result);
         })
         app.delete('/cart/:id', async (req, res) => {
@@ -97,22 +143,29 @@ async function run() {
             const result = await menuCollection.find().toArray();
             res.send(result);
         })
-        app.get('/menu/:category', async (req, res) => {
-            const category = req.params.category;
-            const query = { category: category }
-            if (category === "salad" ||
-                category === "pizza" ||
-                category === "soup" ||
-                category === "dessert" ||
-                category === "offered"
-            ) {
-                const result = await menuCollection.find(query).toArray()
-                res.send(result)
-            } else {
-                const result = await menuCollection.find({}).limit(20).toArray()
-                res.send(result)
-            }
+        app.delete('/menu/:id', verifyJwt, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await menuCollection.deleteOne(query);
+            res.send(result);
         })
+
+        // app.get('/menu/:category', async (req, res) => {
+        //     const category = req.params.category;
+        //     const query = { category: category }
+        //     if (category === "salad" ||
+        //         category === "pizza" ||
+        //         category === "soup" ||
+        //         category === "dessert" ||
+        //         category === "offered"
+        //     ) {
+        //         const result = await menuCollection.find(query).toArray()
+        //         res.send(result)
+        //     } else {
+        //         const result = await menuCollection.find({}).limit(20).toArray()
+        //         res.send(result)
+        //     }
+        // })
 
 
         // Send a ping to confirm a successful connection
